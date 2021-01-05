b5cbe546d8b921eea7fa1284c8f16ace401c97c3
dzb <54801870+CNFierceman@users.noreply.github.com>
2021-01-05 15:32:33 +0800

    第九章课本例题

---
+++ a9_home/StopWatch.java
@@ -0,0 +1,35 @@
+package a9_home;
+
+public class StopWatch 
+{
+
+	// TODO Auto-generated method stub
+	private long startTime;
+	private long endTime;
+	public void StopWatch()
+	{
+		java.util.Date date=new java.util.Date();
+		startTime=date.getTime();
+	}
+	public void start(long startTime)
+	{
+		this.startTime=startTime;
+	}
+	public void stop(long endTime)
+	{
+		this.endTime=endTime;
+	}
+	public long getElapsedTime()
+	{
+		java.util.Date time=new java.util.Date();
+		return time.getTime();
+	}
+	public long getStartTime()
+	{
+		return startTime;
+	}
+	public long getEndTime()
+	{
+		return endTime;
+	}
+}
